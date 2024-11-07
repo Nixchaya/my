8f1a4398aa29cc01d9a447e28353c737c066294f
Nixchaya <52950288+Nixchaya@users.noreply.github.com>
2024-11-06 18:32:40 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mombas</title>
+</head>
+<body>
+    <h1>hello mombas</h1>
+    <h1>hello 123232tsorpe maaaombas</h1>
+
+</body>
+</html>
